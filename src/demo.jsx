df2e1a1659f5d604bd7facb6c18df157dc93260c
coder-cwc <17502088283@163.com>
2023-12-06 17:41:56 +0800

    feat: 树组件三层滚动分页加载的实现 — 第一版

--- src/demo.jsx
+++ src/demo.jsx
@@ -3,31 +3,43 @@ import axios from "axios";
 import clbIcon from "./assets/clb.png";
 import listenerIcon from "./assets/listener.png";
 import domainIcon from "./assets/domain.png";
+import { throttle } from "lodash";
 
 export default defineComponent({
   name: "TreeDemo",
   setup() {
     const treeData = reactive([]);
-    const loadingRef = ref([]);
+    const loadingRef = ref(null);
 
+    // 最外层 clb 的分页参数
+    let clbPageCount = 1;
+
+    // Intersection Observer 监听器
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          console.log('加载数据');
+          // 触发 loadingRef 身上的 loadData 自定义事件
+          loadingRef.value.$emit("loadData");
         }
       });
     });
 
+    // 获取 clb 的数据
     const getInitData = async () => {
       const [res1, res2] = await Promise.all([
-        axios.get("http://localhost:3000/clb", { params: { _page: 1, _limit: 50 } }),
+        axios.get("http://localhost:3000/clb", { params: { _page: clbPageCount, _limit: 50 } }),
         axios.get("http://localhost:3000/clb"),
       ]);
-      Object.assign(treeData, res1.data.map(item => {
+      Object.assign(treeData, [...treeData, ...res1.data.map(item => {
         item.type = "clb";
         item.async = true;
+        // 每个 clb 身上的 pageCount 控制其下 listener 的请求页码
+        item.pageCount = 1;
         return item;
-      }))
+      })])
+      if(treeData.length < res2.data.length) {
+        treeData.push({type: "loading"});
+      }
     }
 
     onMounted(() => {
@@ -37,31 +49,31 @@ export default defineComponent({
     const getRemoteData = async (_item, _callback, _schema) => {
       if (_item.type === 'clb') {
         const [res1, res2] = await Promise.all([
-          axios.get(`http://localhost:3000/listener?pid=${_item.id}`, { params: { _page: 1, _limit: 50 } }),
+          axios.get(`http://localhost:3000/listener?pid=${_item.id}`, { params: { _page: _item.pageCount, _limit: 50 } }),
           axios.get(`http://localhost:3000/listener?pid=${_item.id}`)
         ]);
-        _item.children = res1.data.map(item => {
+        _item.children = [..._item.children, ...res1.data.map(item => {
           item.type = "listener";
           item.async = true;
+          // 每个 listener 身上的 pageCount 控制其下 domain 的请求页码
+          item.pageCount = 1;
           return item;
-        });
+        })];
         if (_item.children.length < res2.data.length) {
           // 插入 loading 节点
-          _item.children.push({type: "loading"});
+          _item.children.push({type: "loading", _parent: _item});
         }
-        console.log(loadingRef.value);
       } else if (_item.type === 'listener') {
         const [res1, res2] = await Promise.all([
-          axios.get(`http://localhost:3000/domain?pid=${_item.id}`, { params: { _page: 1, _limit: 50 } }),
+          axios.get(`http://localhost:3000/domain?pid=${_item.id}`, { params: { _page: _item.pageCount, _limit: 50 } }),
           axios.get(`http://localhost:3000/domain?pid=${_item.id}`)
         ]);
-        _item.children = res1.data.map(item => {
+        _item.children = [..._item.children, ...res1.data.map(item => {
           item.type = "domain";
-          item.async = true;
           return item;
-        });
+        })];
         if (_item.children.length < res2.data.length) {
-          _item.children.push({type: "loading"});
+          _item.children.push({type: "loading", _parent: _item});
         }
       }
     }
@@ -69,9 +81,16 @@ export default defineComponent({
     return () => (
       <div style={{ width: "500px", height: "100vh" }}>
         <bk-tree
+          onScroll={throttle(() => {
+            if (loadingRef.value) {
+              // 监听 loadingRef
+              observer.observe(loadingRef.value.$el);
+            }
+          }, 300)}
           data={treeData}
           level-line
           label="name"
+          virtual-render
           children="children"
           async={{
             callback: getRemoteData,
@@ -80,7 +99,25 @@ export default defineComponent({
         >
           {{
             default: ({ data }) => {
-              if (data.type === 'loading') return <bk-loading ref={(ref) => loadingRef.value.push(ref)} loading size="small"><div style={{ height: "36px" }}></div></bk-loading>
+              if (data.type === 'loading') {
+                return <bk-loading ref={loadingRef} onLoadData={() => {
+                    // 因为在标签上使用 data-xxx 会丢失引用，但我需要 data._parent 的引用（因为加载数据时会直接操作该对象），所以这里借用了闭包的特性。
+                  if (data._parent) {
+                    // 加载下一页的 listener 或 domain
+                    //1.移除loading节点
+                    data._parent.children.pop();
+                    //2.更新分页参数
+                    data._parent.pageCount++;
+                    //3.请求下一页数据
+                    getRemoteData(data._parent);
+                  } else {
+                    // 加载下一页的 clb
+                    treeData.pop();
+                    clbPageCount++;
+                    getInitData();
+                  }
+                }} loading size="small"><div style={{ height: "36px" }}></div></bk-loading>
+              }
               return <div>{data.name}</div>
             },
             nodeType: (node) => {
